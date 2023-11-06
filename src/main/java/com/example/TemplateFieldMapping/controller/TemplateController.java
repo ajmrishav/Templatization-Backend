c6af1e2549ba42f875779c37393477d894ebb2c6
ajmrishav <rishavoff12@gmail.com>
2023-11-06 19:42:22 +0530

    Create api changes for inserting data

--- src/main/java/com/example/TemplateFieldMapping/controller/TemplateController.java
+++ src/main/java/com/example/TemplateFieldMapping/controller/TemplateController.java
@@ -1,5 +1,30 @@
 package com.example.TemplateFieldMapping.controller;
 
 
+import com.example.TemplateFieldMapping.model.InputFieldsTO;
+import com.example.TemplateFieldMapping.model.UploadDataTO;
+import com.example.TemplateFieldMapping.service.TemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RestController()
 public class TemplateController {
+
+  @Autowired
+  private TemplateService templateService;
+
+    @CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
+    @RequestMapping(value = "/create/orgtable" ,method = RequestMethod.POST)
+  public Boolean createOrganisationTable(@RequestBody InputFieldsTO inputFieldsTO){
+   return templateService.schemaForData(inputFieldsTO);
+  }
+
+  @CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
+  @RequestMapping("/put/data")
+  public void  putDataInOrgTable(@RequestBody UploadDataTO data){
+      templateService.uploadData(data);
+  }
 }
